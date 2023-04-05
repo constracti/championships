2e068f62d4a47f5e95917cd347cc12c3809360e1
Cptlagou <cptlagou@gmail.com>
2023-04-05 17:36:44 +0300

    first changes for structure completion

--- test.js
+++ test.js
@@ -1,23 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function ()
+{
 	console.log('ready');
 
 	const form = document.forms[0];
 
 	const form_teams = form.teams;
 	// example input value for testing
-	form_teams.value = [
-		'Φωτοδότες',
-		'Αγωνιστές',
-		'Τροπαιοφόροι',
-		'Πρόμαχοι',
-	].join('\n');
+	form_teams.value =
+		[
+			'Φωτοδότες',
+			'Αγωνιστές',
+			'Τροπαιοφόροι',
+			'Πρόμαχοι',
+		].join('\n');
 
 	const form_sports = form.sports;
 	// example input value for testing
-	form_sports.value = [
-		'Ποδόσφαιρο',
-		'Μπάσκετ',
-	].join('\n');
+	form_sports.value =
+		[
+			'Ποδόσφαιρο',
+			'Μπάσκετ',
+		].join('\n');
 
 	const form_date_start = form['start'];
 	// example input value for testing
@@ -33,16 +36,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	const form_zones = form['zones'];
 	// example input value for testing
-	form_zones.value = [
-		'Πρωινή',
-		'Απογευματινή',
-	].join('\n');
+	form_zones.value =
+		[
+			'Πρωινή',
+			'Απογευματινή',
+		].join('\n');
 
 	const form_rounds = form['rounds'];
 	// example input value for testing
 	form_rounds.value = 2;
 
-	const points_fn_obj = {
+	const points_fn_obj =
+	{
 		'ποδόσφαιρο': (sh, sa) => {
 			if (sh > sa)
 				return [3, 0];
@@ -69,32 +74,25 @@ document.addEventListener('DOMContentLoaded', function() {
 	let groups = [];
 	let zones = [];
 	let rounds = [];
+	let slots = [];
 
-	let t = document.getElementsByName('teams'); //could optimize that with the above form consts...
-	// TODO that's the point... use constants with descriptive names and access them as form elements
-	let gs = document.getElementsByName('grstructure');
-	let z = document.getElementsByName('zones');
-	let r = document.getElementsByName('rounds');
-	let s = document.getElementsByName('sports');
-	let sd = document.getElementsByName('start');
-	let ed = document.getElementsByName('end');
 
 	// TODO even initializations should be done on the submit event; they are part of the algorith
-	let init_group_structure = gs[0].value.trim().toLowerCase();
-	let init_zone = z[0].value.trim().split('\n');
-	let init_number_of_rounds = r[0].value.trim().toLowerCase();
-	const init_start_date = new Date(sd[0].value);
-	const init_end_date = new Date(ed[0].value);
+
+	const init_start_date = new Date(form_date_start.value);
+	const init_end_date = new Date(form_date_stop.value);
 
 	let difference = (init_end_date.getTime() - init_start_date.getTime()) / (1000 * 60 * 60 * 24);
 
-	form.addEventListener('submit', function(event) {
+	form.addEventListener('submit', function (event)
+	{
 		console.log('submit');
 		event.preventDefault();
 
 		// TODO run algorith here -> Done.
 
-		teams = form_teams.value.split('\n').map(name => name.trim()).filter(name => name.length).map(function(name, id) {
+		teams = form_teams.value.split('\n').map(name => name.trim()).filter(name => name.length).map(function (name, id)
+		{
 			return {
 				id: id,
 				name: name,
@@ -103,7 +101,8 @@ document.addEventListener('DOMContentLoaded', function() {
 		console.log('teams:\n' + teams.map(team => `${team.id}: ${team.name}`).join('\n'));
 		//teams.forEach(team => console.log(`${team.id}: ${team.name}`));
 
-		sports = form_sports.value.split('\n').map(name => name.trim().toLowerCase()).filter(name => name.length).map(function(name, id) {
+		sports = form_sports.value.split('\n').map(name => name.trim().toLowerCase()).filter(name => name.length).map(function (name, id)
+		{
 			return {
 				id: id,
 				name: name,
@@ -112,99 +111,152 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 		console.log('sports:\n' + sports.map(sport => `${sport.id}: ${sport.name}`).join('\n'));
 
-		// TODO try to code in a functional programming style as in teams and sports
-		// produce rounds (aka "slots")
-		for (let d = 0; d <= difference; d++) {
-			for (let i = 0; i < init_zone.length; i++) {
-				zone = // TODO variables not declared with a const/let/var keyword become global; in this case, const is prefered
+		// TODO try to code in a functional programming style as in teams and sports -> Done?
+
+		zones = form_zones.value.split('\n').map(zone => zone.trim()).filter(zone => zone.length).map(function (zone, index)
+		{
+			return {
+				rank: index,
+				name: zone,
+			};
+		});
+		for (let d = 0; d <= difference; d++)
+		{
+			for (let i = 0; i < zones.length; i++)
+			{
+				for (let j = 0; j < form_rounds.value; j++)
 				{
-					id: i,
-					name: init_zone[i] // TODO terminate last line of multiline object / list expressions with a comma for scalability
-				};
-				for (let k = 0; k < sports.length; k++) {
-					for (let j = 0; j < init_number_of_rounds; j++) {
-						round =
-						{
-							sport: sports[k],
-							id: 0,
-							date: new Date(init_start_date.getFullYear(), init_start_date.getMonth(), init_start_date.getDate() + d).toDateString(), // TODO see comments @ https://stackoverflow.com/a/34324394/6884847
-							zone: zone.id, // IDEA you may reference the object; it's just a pointer!
-							rank: j,
-							available: true
-						};
-						rounds.push(round);
-					}
+					round =
+					{
+						rank: 0,
+						date: new Date(init_start_date.getFullYear(), init_start_date.getMonth(), init_start_date.getDate() + d).toDateString(), // TODO see comments @ https://stackoverflow.com/a/34324394/6884847
+						zone: zones[i], // IDEA you may reference the object; it's just a pointer! -> Done.
+					};
+					rounds.push(round);
 				}
-				zones.push(zone)
 			}
 		}
-		for (let i = 0; i < rounds.length; i++) {
-			rounds[i]['id'] = i;
+		for (let i = 0; i < rounds.length; i++)
+		{
+			rounds[i]['rank'] = i;
+			
+			for (let k = 0; k < sports.length; k++)
+			{
+				slot =
+				{
+					sport: sports[k],
+					round: rounds[i],
+					available: true,
+					match: null,
+				};
+				slots.push(slot);
+			}
+			
+			for (let k = 0; k < sports.length; k++)
+			{
+				rounds[i][sports[k].name]=[];
+				for (let j = 0; j < slots.length; j++)
+				{
+					if (slots[j].sport.name === sports[k].name && slots[j].round.rank===i)
+					{
+						rounds[i][sports[k].name].push(slots[j]);
+					}	
+				}
+			}
 		}
 
 
-		if (init_group_structure === 'default') {
-			for (let k = 0; k < sports.length; k++) {
+		if (form_grstructure.value === 'default')//all teams in one group 
+		{
+			for (let k = 0; k < sports.length; k++)
+			{
 				group =
 				{
 					sport: sports[k],
 					teams: teams,
-					structure: 'default'
+					structure: 'default',
 				};
 				groups.push(group)
 				// TODO cleaner approach: (i = 0; i < length) (j = i + 1; j < length)
-				for (let i = 0; i < group.teams.length - 1; i++) {
-					for (let j = i; j < group.teams.length - 1; j++) {
-						match = // TODO use null for unset values (sh, sa, round)
+				for (let i = 0; i < group.teams.length - 1; i++)
+				{
+					for (let j = i; j < group.teams.length - 1; j++)
+					{
+						match = // TODO use null for unset values (sh, sa, round) -> Done.
 						{
 							th: group.teams[i],
 							ta: group.teams[j + 1],
-							sh: 0,
-							sa: 0,
-							round: -1
+							sh: null,
+							sa: null,
+							//slot: null,
+							//i would like to include 'sport' here.
 						};
 						matches.push(match);
 					}
 				}
 			}
-			for (let m = 0; m < matches.length; m++) {
-				for (let r = 0; r < rounds.length; r++) {
-					if (rounds[r].available === true)//if there is an empty court
-					{
-						let team1 = matches[m].th;
-						let team2 = matches[m].ta;
-						let scheduled = false;//If in this specific date, in a specific zone and rank a team is scheduled to play something else.
 
-						for (let rm = 0; rm < matches.length; rm++) { // TODO more efficient match grouping
-							if (matches[rm].round !== -1) {
-								if (rounds[r].date === rounds[matches[rm].round].date && rounds[r].rank === rounds[matches[rm].round].rank && rounds[r].zone === rounds[matches[rm].round].zone && (matches[rm].th === team1 || matches[rm].ta === team1 || matches[rm].th === team2 || matches[rm].ta === team2)) {
-									scheduled = true;
-									break;
+			//Here is the scheduling for default structure. Working on making it a recursive function that every time a match is placed in a slot, it calls itself after it pops the match, to schedule the next one until all matches are placed in a slot.
+			for (let r = 0; r < rounds.length; r++)//for every round
+			{
+				for (let s = 0; s < slots.length; s++)//for every slot in this round
+				{
+					for (let m = 0; m < matches.length; m++)
+					{
+						if (slots[s].available === true)//if there is an empty court
+						{
+							let team1 = matches[m].th;
+							let team2 = matches[m].ta;
+							let scheduled = false;//If in this specific date, in a specific zone and rank a team is scheduled to play something else (1)
+
+							for (let sp = 0; sp < sports.length; sp++)
+							{
+								for (let j = 0; j < rounds[r][sports[sp].name].length; j++)
+								{
+									if (rounds[r][sports[sp].name][j].match !== null)
+									{
+										if (rounds[r][sports[sp].name][j].match.th === team1 || rounds[r][sports[sp].name][j].match.ta === team1 || rounds[r][sports[sp].name][j].match.th === team2 || rounds[r][sports[sp].name][j].match.ta === team2)
+										{
+											scheduled = true;
+											break;
+										}
+									}
+									if (scheduled)
+									{
+										break;
+									}
 								}
 							}
+
+
+							if (!scheduled)
+							{
+								slots[s].match = matches[m];
+								slots[s].available = false;
+								break;
+							}
 						}
-						if (!scheduled) {
-							matches[m].round = rounds[r].id;
-							rounds[r].available = false;
+						else
+						{
 							break;
 						}
 					}
 				}
 			}
-			for (let m = 0; m < matches.length; m++) {//not finished
-				if (matches[m].round === -1)
-				{
-					alert('No solution found, try to decrease the number of teams or extend the period of the schedule.\
-						\n\nKeep in mind that for the time being the scheduling algorithm is nowhere near its final state and cannot find a good solution.');
-					break;
-				}
+		}
+		for (let m = 0; m < matches.length; m++) 
+		{//not finished
+			if (matches[m].round === -1) 
+			{
+				alert('No solution found, try to decrease the number of teams or extend the period of the schedule.\
+					\n\nKeep in mind that for the time being the scheduling algorithm is nowhere near its final state and cannot find a good solution.');
+				break;
 			}
 		}
-
-		console.log(matches);
-		console.log(groups);
-		console.log(rounds);
 		// TODO output to console or html in a human-readable format, as in the excel printable tab
+		console.table(matches);
+		console.table(groups);
+		console.table(slots);
 	});
 });
 
